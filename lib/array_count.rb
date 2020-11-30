d27a9c53b817cddff606cd1efe28387b0bafd08d
nick repasy <nrepasy@gmail.com>
2020-11-30 06:30:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,12 +3,17 @@ total= 0
  array.count do |i|
    
    if i.is_a? String
-     total=+1
+     total+=1
 end
 end
 end
 
 def count_empty_strings(array)
- 
+ total= 0
+ array.count do |i|
+   
+   if i.is_a? String
+     String.empty?
+     total_
 
 end
