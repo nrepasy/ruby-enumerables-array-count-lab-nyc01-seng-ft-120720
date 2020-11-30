e800e2fc16d62b24863cc917655ff0a318d9873c
nick repasy <nrepasy@gmail.com>
2020-11-30 03:56:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,4 +5,8 @@ array.count do |element|
     total += 1 
   else 
 end
+end
 
+def count_empty_strings(array)
+  # Return the total number of EMPTY strings in the provided array using the count enumerable
+end
